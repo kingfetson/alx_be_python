226d9d6af34534d3d467726cab9f7609f0906988
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-11-19 13:35:33 +0300

    Create list of favorite fruits and access the second element for printing

---
+++ python_week3/favorite_fruits_list.py
@@ -0,0 +1,8 @@
+# Exercise 1: Favorite fruits list
+
+# Create a list of favorite fruits
+fruits = ["apple", "banana", "mango", "orange"]
+
+# Access the second element (index 1) and print it
+print(fruits[1])
+print(fruits[0])
